d195c00c7416d6a9fb8c95f07d396bce74fb392d
jayantjichkar787-ux <jayantjichkar787@gmail.com>
2026-08-14 16:42:31 +0530

    Add files via upload

---
+++ Tests/TEST 1/TEST 2/Que_1.py
@@ -0,0 +1,20 @@
+# 1. Write a program to print first n prime numbers
+
+n = int(input("Enter n: "))
+
+count = 0
+num = 2
+
+while (count < n):
+    prime = True
+
+    for i in range(2, num):
+        if (num % i == 0):
+            prime = False
+            break
+
+    if prime:
+        print(num, end=" ")
+        count += 1
+
+    num += 1
